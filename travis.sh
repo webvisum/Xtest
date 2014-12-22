e240142f88d0c351e0d1164bd5b48198c55c6ead
Tobias Vogt <tobias@code-x.de>
2014-12-23 00:17:18 +0100

    Custom Travis-Script

---
+++ travis.sh
@@ -0,0 +1,30 @@
+set -e
+set -x
+
+# check if this is a travis environment
+function cleanup {
+  echo "Removing build directory ${BUILDENV}"
+  rm -rf ${BUILDENV}
+}
+
+trap cleanup EXIT
+
+if [ ! -z $TRAVIS_BUILD_DIR ] ; then
+  WORKSPACE=$TRAVIS_BUILD_DIR
+fi
+
+if [ -z $WORKSPACE ] ; then
+  echo "No workspace configured, please set your WORKSPACE environment"
+  exit
+fi
+
+BUILDENV=`mktemp -d /tmp/mageteststand.XXXXXXXX`
+
+echo "Using build directory ${BUILDENV}"
+
+git clone https://github.com/AOEpeople/MageTestStand.git ${BUILDENV}
+cp -rf ${WORKSPACE} ${BUILDENV}/.modman/
+${BUILDENV}/install.sh
+
+cd ${BUILDENV}/htdocs/tests
+php phpunit.phar --colors -d display_errors=1 completeUnitSuite.php
