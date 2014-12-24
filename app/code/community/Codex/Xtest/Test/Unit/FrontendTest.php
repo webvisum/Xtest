c616b59329b00ba8ea4edb868b6962299d4a75b9
Tobias Vogt <tobias@code-x.de>
2014-12-24 22:41:52 +0100

    Fixed: Missing Translation

--- app/code/community/Codex/Xtest/Test/Unit/FrontendTest.php
+++ app/code/community/Codex/Xtest/Test/Unit/FrontendTest.php
@@ -7,7 +7,7 @@ class Codex_Xtest_Test_Unit_FrontendTest extends Codex_Xtest_Xtest_Unit_Frontend
     {
         $this->dispatch('customer/account/login');
 
-        $this->assertContains('Login or Create an Account', $this->getResponseBody() );
+        $this->assertContains( Mage::helper('customer')->__('Login or Create an Account'), $this->getResponseBody() );
         $this->assertInstanceOf('Mage_Page_Block_Html', $this->getLayout()->getBlock('root') );
     }
 
@@ -15,7 +15,7 @@ class Codex_Xtest_Test_Unit_FrontendTest extends Codex_Xtest_Xtest_Unit_Frontend
     {
         $this->dispatchUrl( Mage::getUrl('customer/account/login') );
 
-        $this->assertContains('Login or Create an Account', $this->getResponseBody() );
+        $this->assertContains( Mage::helper('customer')->__('Login or Create an Account'), $this->getResponseBody() );
         $this->assertInstanceOf('Mage_Page_Block_Html', $this->getLayout()->getBlock('root') );
     }
 
