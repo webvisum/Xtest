db71b455457775e52c495fb80f796917e886ea85
Tobias Vogt <tobias@code-x.de>
2014-12-28 20:12:19 +0100

    Test Mocking Helper + Singleton

--- app/code/community/Codex/Xtest/Test/Integration/Mocking.php
+++ app/code/community/Codex/Xtest/Test/Integration/Mocking.php
@@ -23,12 +23,42 @@ class Codex_Xtest_Test_Integration_Mocking extends Codex_Xtest_Xtest_Unit_Fronte
 
     public function testHelperMock()
     {
-        $this->markTestIncomplete();
+        $testReturn = 'http://www.webguys.de';
+
+        $mock = $this->getHelperMock('catalog/product', array('getProductUrl') );
+        $mock->expects($this->any())
+            ->method('getProductUrl')
+            ->will($this->returnValue( $testReturn ));
+
+        $this->assertEquals( $testReturn, $mock->getProductUrl() );
+
+        $this->addHelperMock('catalog/product', $mock );
+
+        $product = Mage::helper('catalog/product');
+        $this->assertInstanceOf( get_class($mock), $product );
+        $this->assertEquals( $product, $mock );
     }
 
     public function testSingletonMock()
     {
-        $this->markTestIncomplete();
+        $testReturn = 'Test-Name';
+
+        $mock = $this->getModelMock('catalog/product', array('getName') );
+        $mock->expects($this->any())
+            ->method('getName')
+            ->will($this->returnValue( $testReturn ));
+
+        $this->assertEquals( $testReturn, $mock->getName() );
+
+        $this->addModelMock('catalog/product', $mock );
+
+        $product = Mage::getSingleton('catalog/product');
+        $this->assertInstanceOf( get_class($mock), $product );
+        $this->assertEquals( $product, $mock );
+        $product->setId( 1000 );
+
+        $product = Mage::getSingleton('catalog/product');
+        $this->assertEquals( 1000, $product->getId() );
     }
 
     public function testBlockMock()
