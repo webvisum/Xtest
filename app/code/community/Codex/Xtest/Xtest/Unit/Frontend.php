26286cc070ba5d5a772926f8ba7b6885466adbe5
Tobias Vogt <tobias@code-x.de>
2015-01-07 17:18:54 +0100

    Added method to make custom Quote available for Magento Core classes

--- app/code/community/Codex/Xtest/Xtest/Unit/Frontend.php
+++ app/code/community/Codex/Xtest/Xtest/Unit/Frontend.php
@@ -8,6 +8,12 @@ class Codex_Xtest_Xtest_Unit_Frontend extends Codex_Xtest_Xtest_Unit_Abstract
         Xtest::initFrontend();
     }
 
+    /**
+     * Check if payment methdos are available
+     *
+     * @param Mage_Sales_Model_Quote $quote
+     * @param array $allowedMethods
+     */
     public function assertPaymentMethodsAvailable( Mage_Sales_Model_Quote $quote, Array $allowedMethods )
     {
         $availableMethods = array();
@@ -22,6 +28,19 @@ class Codex_Xtest_Xtest_Unit_Frontend extends Codex_Xtest_Xtest_Unit_Abstract
         sort($allowedMethods);
         sort($availableMethods);
         $this->assertEquals($allowedMethods, $availableMethods);
+    }
+
+    /**
+     * Makes Quote available for Magento Core classes
+     *
+     * @param Mage_Sales_Model_Quote $quote
+     */
+    public function populuateQuote( Mage_Sales_Model_Quote &$quote )
+    {
+        $quote->save();
+        $quote = Mage::getModel('sales/quote')->load( $quote->getId() );
+        Mage::getSingleton('checkout/cart')->setQuote( $quote );
+        Mage::getSingleton('checkout/session')->setQuoteId( $quote->getId() );
     }
 
 }
